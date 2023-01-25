8719207a517f12b838c8da2c8ae824bc7d376492
Meet Patel <81353451+mkpatel-247@users.noreply.github.com>
2023-01-25 13:25:40 +0530

    Create 1603. Design Parking System.java

---
+++ Programming Skills - 1/Day - 12 Class & Object/1603. Design Parking System.java
@@ -0,0 +1,17 @@
+class ParkingSystem {
+    
+    private int[] parking;
+    public ParkingSystem(int big, int medium, int small) {
+        parking = new int[]{0, big, medium, small};
+    }
+    
+    public boolean addCar(int carType) {
+        return parking[carType]-- > 0;
+    }
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem obj = new ParkingSystem(big, medium, small);
+ * boolean param_1 = obj.addCar(carType);
+ */
