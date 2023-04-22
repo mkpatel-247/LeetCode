63d3cf84b0b764ce069cdac040c165392da304e6
Meet Patel <81353451+mkpatel-247@users.noreply.github.com>
2023-04-22 23:49:58 +0530

    Update 217. Contains Duplicate.java

--- Data Structure I/Day 01/217. Contains Duplicate.java
+++ Data Structure I/Day 01/217. Contains Duplicate.java
@@ -4,6 +4,7 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
         Hashtable<Integer, Boolean> ht = new Hashtable<>();
         for(int i=0;i<nums.length;i++){
+ 
             if(ht.get(nums[i]) != null)
                 return true;
             ht.put(nums[i],true);
